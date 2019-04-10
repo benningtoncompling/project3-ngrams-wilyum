8bf534822fd2550152cc2c4d722656d9a360d8a1
William Bowers <williambowers@bennington.edu>
2019-04-10 00:53:05 -0400

    probability issues

--- build_ngram_model.py
+++ build_ngram_model.py
@@ -6,7 +6,6 @@ takes in an input file and outputs a file with the probabilities for each unigra
 3/25/2019
 '''
 import sys
-import nltk
 import math
 
 input_file = sys.argv[1]
@@ -32,6 +31,7 @@ def unigrams():
 				word_dict[word] = 1
 
 	unigram_types = len(word_dict)
+
 	#sort unigrams by abc's then by value
 	unigrams_sorted_abc = sorted(word_dict, key=lambda x: x)
 	unigrams_sorted = sorted(unigrams_sorted_abc, key=lambda x: word_dict[x], reverse = True)
@@ -46,6 +46,7 @@ def unigrams():
 	
 	return unigrams_list, unigram_types, unigram_tokens
 
+#Make bigrams w/o nltk
 bigram_dict = {}
 def bigrams():
 	bigram_list = []
@@ -74,36 +75,91 @@ def bigrams():
 			bigram_dict[bigram] += 1
 		else:
 			bigram_dict[bigram] = 1
+
 	bigram_types = len(bigram_dict)
 
-#sort bigrams by abc's then by value
+
+	#sort bigrams by abc's then by value
 	bigrams_sorted_abc = sorted(bigram_dict, key=lambda x: x)
 	bigrams_sorted = sorted(bigrams_sorted_abc, key=lambda x: bigram_dict[x], reverse = True)
+
 	
+	#getting onegram values to use for bigram probabailty
 	onegrams_list = [word[0] for word in bigram_list] #should be equal to the original unigram_token count but isnt
 	onegrams_dict = {}
 	for onegram in onegrams_list:
 		if onegram in onegrams_dict:
-			onegram_dict[onegram] += 1
+			onegrams_dict[onegram] += 1
 		else:
-			onegram_dict[onegram] = 1
+			onegrams_dict[onegram] = 1
+	
+	
 
 
 	bigrams_list = []
 	for bigram in bigrams_sorted:
 		bigram_count = bigram_dict[bigram]
-		probability = bigram_count/   # this is wrong, how take the count of the first element in each bigram
+		print(bigram_count)
+		onegram_count = onegrams_dict[bigram[0]] 
+		print(onegram_count)
+		probability = bigram_count/onegram_count
 		bigram_log = math.log10(probability)
 		to_append = (bigram_count, probability, bigram_log, bigram)
 		bigrams_list.append(to_append)
+
 	return bigrams_list, bigram_types, bigram_tokens
 
 
 
 trigram_dict = {}
 def trigrams():
-	return
-	#return trigrams_list, trigram_types, trigram_tokens
+	trigram_list = []
+	flattened_trigram_list = []
+	trigram_tokens = 0
+
+	for line in words:
+		for word in range(len(line)-2):
+			trigram_list.append([[line[word]], [line[word+1]], [line[word+2]]])
+
+	#from [['<s>'], ['i'], ['a']] to ('<s>', 'i', 'a') to use tuples as dict keys
+	for x in trigram_list:
+		for y in x:
+			flattened_trigram_list.append(y)
+	y= [''.join(x) for x in flattened_trigram_list]
+	trigram_list = list(zip(*[iter(y)] * 3))
+
+	for trigram in trigram_list:
+		trigram_tokens += 1
+		if trigram in trigram_dict:
+			trigram_dict[trigram] += 1
+		else:
+			trigram_dict[trigram] = 1
+	trigram_types = len(trigram_dict)
+
+	#sort bigrams by abc's then by value
+	trigrams_sorted_abc = sorted(trigram_dict, key=lambda x: x)
+	trigrams_sorted = sorted(trigrams_sorted_abc, key=lambda x: trigram_dict[x], reverse = True)
+	
+	#getting twograms to use for the trigram probability
+	twograms_list = [word[0] for word in trigram_list] #should be equal to the original unigram_token count but isnt
+	twograms_dict = {}
+	for twogram in twograms_list:
+		if twogram in twograms_dict:
+			twograms_dict[twogram] += 1
+		else:
+			twograms_dict[twogram] = 1
+
+
+	trigrams_list = []
+	for trigram in trigrams_sorted:
+		trigram_count = trigram_dict[trigram]
+		twogram_count = twograms_dict[trigram] #need to get first 2 values of touple
+		probability = trigram_count/twogram_count
+		trigram_log = math.log10(probability)
+		to_append = (trigram_count, probability, trigram_log, trigram)
+		trigrams_list.append(to_append)
+
+	return trigrams_list, trigram_types, trigram_tokens
 
 #WRITE OUTPUTS TO FILE
 def writer(unigrams_list, unigram_types, unigram_tokens, bigrams_list, bigram_types, bigram_tokens):
@@ -113,7 +169,7 @@ def writer(unigrams_list, unigram_types, unigram_tokens, bigrams_list, bigram_ty
 		output.write('\\data\\\n')
 		output.write('ngram 1: type=' + str(unigram_types) + ' token=' + str(unigram_tokens) + '\n')
 		output.write('ngram 2: type=' + str(bigram_types) + ' token=' + str(bigram_tokens) + '\n' + '\n')
-		#output.write('ngram 3: type=' + str(trigram_types) + ' token=' + str(trigram_tokens) + '\n' + '\n')
+		output.write('ngram 3: type=' + str(trigram_types) + ' token=' + str(trigram_tokens) + '\n' + '\n')
 
 		#unigrams
 		output.write('\\1-grams:' + '\n')
@@ -126,16 +182,17 @@ def writer(unigrams_list, unigram_types, unigram_tokens, bigrams_list, bigram_ty
 		for line in bigrams_list:
 			line = ' '.join(map(str, line))
 			output.write(str(line) + '\n')
+
 		#trigrams
-		'''output.write('\\3-grams:' + '\n')
+		output.write('\\3-grams:' + '\n')
 		for line in trigrams_list:
 			line = ' '.join(map(str, line))
-			output.write(str(line) + '\n')'''
+			output.write(str(line) + '\n')
 		output.close()
 
-unigrams_list, unigram_types, unigram_tokens = unigrams()
-bigrams_list, bigram_types, bigram_tokens = bigrams()
+#unigrams_list, unigram_types, unigram_tokens = unigrams()
+#bigrams_list, bigram_types, bigram_tokens = bigrams()
+bigrams()
 #trigrams_list, trigram_types, trigram_tokens = trigrams()
-writer(unigrams_list, unigram_types, unigram_tokens, bigrams_list, bigram_types, bigram_tokens)
-
-
+#writer(unigrams_list, unigram_types, unigram_tokens, bigrams_list, bigram_types, bigram_tokens)
+#writer(bigrams_list, bigram_types, bigram_tokens)
